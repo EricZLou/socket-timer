ec584b939ce9fa4189822f5a1bb096224d3e12f9
ezl-13 <ericzlou@gmail.com>
2020-12-26 15:25:22 -0500

    Add path package

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const server = require('http').createServer(app);
 const io = module.exports.io = require("socket.io")(server, {
   cors: {
